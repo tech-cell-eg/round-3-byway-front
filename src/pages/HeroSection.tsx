a3b9ae0a919ec0cbcbd04651745fbf8ff0d46ab0
moelsayed011 <me6758478@gmail.com>
2025-04-22 18:06:06 +0200

    feat:add hero section

---
+++ src/pages/HeroSection.tsx
@@ -0,0 +1,34 @@
+import HeroLeft from '@/components/Hero/HeroLeft';
+import HeroRightImage from '@/components/Hero/HeroRightImage';
+
+const HeroSection = () => {
+  return (
+    <>
+      <main className="w-full lg:min-h-[600px] bg-surface-light-primary">
+        <section
+          className="
+          grid 
+          grid-cols-1 
+          md:grid-cols-2 
+          gap-6 
+          items-center 
+          justify-items-center 
+          responsive-primary-padding-x 
+          responsive-primary-padding-y 
+          h-full
+        "
+        >
+          <div className="flex justify-center md:justify-start w-full">
+            <HeroLeft />
+          </div>
+
+          <div className="flex justify-center md:justify-end w-full">
+            <HeroRightImage />
+          </div>
+        </section>
+      </main>
+    </>
+  );
+};
+
+export default HeroSection;
