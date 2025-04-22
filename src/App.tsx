eb15787bbc3ad85b7db52bc16aedf41624a8c88e
moelsayed011 <me6758478@gmail.com>
2025-04-22 18:09:11 +0200

    chore:render hero section

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
+import HeroSection from './pages/HeroSection';
+
 function App() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-svh">
-      <h1 className="text-3xl font-bold underline text-red-500">
-        Hello world!
-      </h1>
-    </div>
+    <>
+      <HeroSection />
+    </>
   );
 }
 
