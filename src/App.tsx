801415ad5c94d9cecdc2ede9de23b3bfafe854c2
moelsayed011 <me6758478@gmail.com>
2025-04-23 22:01:15 +0200

    chore:eidt stylelint

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { createBrowserRouter, RouterProvider } from 'react-router';
 import { Layout } from './pages/Layout';
-import Home from './pages/Home';
 import { queryClient } from './api/queryClient';
 import { QueryClientProvider } from '@tanstack/react-query';
+import Home from './pages/Home';
 
 function App() {
   const router = createBrowserRouter(
@@ -10,17 +10,7 @@ function App() {
       {
         path: '',
         element: <Layout />,
-        children: [
-          { index: true, element: <Home /> },
-          {
-            path: 'signup',
-            element: (
-              <ProtAuth>
-                <SignUp />
-              </ProtAuth>
-            ),
-          },
-        ],
+        children: [{ index: true, element: <Home /> }],
       },
     ]
     // { basename: "/round-3-byway-front" }
