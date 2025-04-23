5e6ddc1957c69f4220e424b3bfa65c1a6a94a590
moelsayed011 <me6758478@gmail.com>
2025-04-23 23:18:19 +0200

    chore:add array of statistic

--- src/pages/StatisticsSection.tsx
+++ src/pages/StatisticsSection.tsx
@@ -1,31 +1,37 @@
 import Statistics from '@/components/Statistics/Statistics';
 
 const StatisticsSection = () => {
+  const statistics = [
+    {
+      number: 250,
+      text: 'Number of instructors.',
+    },
+    {
+      number: 15,
+      text: 'Courses by our best mentors',
+    },
+    {
+      number: 1000,
+      text: 'Number of categories.',
+    },
+    {
+      number: 4500,
+      text: 'Number of students.',
+    },
+  ];
   return (
     <>
       <div
         className="bg-surface-light-secondary min-h-[149px] responsive-primary-padding-x 
             responsive-primary-padding-y grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5"
       >
-        <Statistics
-          number_of_courses={250}
-          title="Courses by our best mentors ."
-          className="lg:border-r-4"
-        />
-        <Statistics
-          number_of_courses={1000}
-          title="Courses by our best mentors ."
-          className="lg:border-r-4"
-        />
-        <Statistics
-          number_of_courses={15}
-          title="Courses by our best mentors ."
-          className="lg:border-r-4"
-        />
-        <Statistics
-          number_of_courses={2400}
-          title="Courses by our best mentors ."
-        />
+        {statistics.map((statistic, index) => (
+          <Statistics
+            key={index}
+            number_of_courses={statistic.number}
+            title={statistic.text}
+          />
+        ))}
       </div>
     </>
   );
