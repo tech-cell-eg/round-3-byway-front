9587bc2fc32bb193f9243ea02526e638b4b65bc9
Mohamed Magdy <93835752+moamed018@users.noreply.github.com>
2025-04-26 01:03:11 +0300

    Update accordion.tsx

--- src/components/ui/accordion.tsx
+++ src/components/ui/accordion.tsx
@@ -23,23 +23,48 @@ function AccordionItem({
   );
 }
 
+type Props = {
+  payment?: boolean;
+};
+
 function AccordionTrigger({
   className,
   children,
+  payment,
   ...props
-}: React.ComponentProps<typeof AccordionPrimitive.Trigger>) {
+}: React.ComponentProps<typeof AccordionPrimitive.Trigger> & Props) {
   return (
     <AccordionPrimitive.Header className="flex">
       <AccordionPrimitive.Trigger
         data-slot="accordion-trigger"
         className={cn(
-          'focus-visible:border-ring focus-visible:ring-ring/50 flex flex-1 items-start justify-between gap-4 rounded-md py-4 text-left text-sm font-medium transition-all outline-none hover:underline focus-visible:ring-[3px] disabled:pointer-events-none disabled:opacity-50 [&[data-state=open]>svg]:rotate-180',
+          'group focus-visible:border-ring focus-visible:ring-ring/50 flex flex-1 items-start justify-between gap-4 rounded-md py-4 text-left text-sm font-medium transition-all outline-none hover:underline focus-visible:ring-[3px] disabled:pointer-events-none disabled:opacity-50 [&[data-state=open]>svg]:rotate-180',
           className
         )}
         {...props}
       >
-        {children}
-        <ChevronDownIcon className="text-muted-foreground pointer-events-none size-4 shrink-0 translate-y-0.5 transition-transform duration-200" />
+        {payment && (
+          <div className="flex gap-2.5 items-center justify-between w-full">
+            <div
+              className={cn(
+                'size-5 rounded-full border border-button-disabled transition-colors flex justify-center items-center'
+              )}
+            >
+              <div
+                className={cn(
+                  'size-3 rounded-full transition-colors group-data-[state=open]:bg-accent-primary'
+                )}
+              ></div>
+            </div>
+            <div className="w-full flex items-center justify-between">
+              {children}
+            </div>
+          </div>
+        )}
+        {!payment && children}
+        {!payment && (
+          <ChevronDownIcon className="text-muted-foreground pointer-events-none size-4 shrink-0 translate-y-0.5 transition-transform duration-200" />
+        )}
       </AccordionPrimitive.Trigger>
     </AccordionPrimitive.Header>
   );
