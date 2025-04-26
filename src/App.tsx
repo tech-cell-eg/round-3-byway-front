847754ab8f8b7c672f0f5a7b3c67c955ebdb8c76
moelsayed011 <me6758478@gmail.com>
2025-04-26 15:10:28 +0300

    chore:fixed accordion

--- src/App.tsx
+++ src/App.tsx
@@ -55,8 +55,8 @@ function App() {
           {
             path: 'cart',
             element: <CartPage />,
-           },
-           {
+          },
+          {
             path: 'instructor',
             element: <InstructorPage />,
           },
