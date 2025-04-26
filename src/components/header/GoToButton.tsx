b1228f87e9e9ffb6860cd333e5c469af4be6558c
AhmedEssam <ae.devverse@gmail.com>
2025-04-26 03:33:21 +0300

    Chore: Set the direction of button to 'ltr' even if the language change

--- src/components/header/GoToButton.tsx
+++ src/components/header/GoToButton.tsx
@@ -2,15 +2,23 @@ import { Link } from 'react-router'; // updated import for react-router-dom
 
 type GoToButtonProps = {
   to: string;
+  dir?: string;
   role?: string;
   value?: string;
   className: string;
   children?: React.ReactNode;
 };
 
-function GoToButton({ to, role, value, className, children }: GoToButtonProps) {
+function GoToButton({
+  to,
+  dir,
+  role,
+  value,
+  className,
+  children,
+}: GoToButtonProps) {
   return (
-    <Link to={to} role={role} className={className}>
+    <Link to={to} dir={dir} role={role} className={className}>
       {value}
       {children}
     </Link>
