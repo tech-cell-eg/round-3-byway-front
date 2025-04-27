83199b77424b264874119a5a1ec2c565fb0efd14
rawan ayman <rawanrooney229@gmail.com>
2025-04-27 22:35:32 +0300

    feat: Add Input Form in Profile Page

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,7 @@ import InstructorPage from './components/InstructorPage/Instructor';
 import CourseDetails from './pages/CourseDetails';
 import { Provider } from 'react-redux';
 import { store } from './Redux/store';
+import ProfilePage from './pages/ProfilePage';
 
 function App() {
   const { i18n } = useTranslation();
@@ -61,6 +62,10 @@ function App() {
           {
             path: 'instructor',
             element: <InstructorPage />,
+          },
+          {
+            path: 'profilepage',
+            element: <ProfilePage />,
           },
         ],
       },
