116c22f813e90135f0cc584ed7f8f2794781b687
AhmedEssam <ae.devverse@gmail.com>
2025-04-30 21:00:16 +0300

    Fix: Remove '/dashboard' from to attribute from Link

--- src/components/CommonComponents/Logo.tsx
+++ src/components/CommonComponents/Logo.tsx
@@ -4,12 +4,16 @@ import { Link } from 'react-router';
 
 function Logo() {
   return (
-    <Link to="/dashboard">
+    <Link to="/">
       <div className="flex items-center">
-        <div className="w-ft">
-          <img src={logoImage} alt="logo" className="w-full object-cover" />
+        <div className="w-[25px] h-[35px] lg:w-[30px] lg:h-[40px]">
+          <img
+            src={logoImage}
+            alt="logo"
+            className="w-full h-full object-cover"
+          />
         </div>
-        <span className="text-content-dark">{appName}</span>
+        <span className="hidden lg:block text-content-dark">{appName}</span>
       </div>
     </Link>
   );
