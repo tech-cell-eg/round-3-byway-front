c0b3da28fd60974fbd8e77b4959fb2332416691e
AhmedEssam <ae.devverse@gmail.com>
2025-04-30 22:04:48 +0300

    Feat: Create CopyUrlButton component that copies the link of the current page

---
+++ src/components/CommonComponents/CopyUrlButton.tsx
@@ -0,0 +1,32 @@
+import { useLocation } from 'react-router';
+import { Button } from '../ui/button';
+import { HTMLAttributes } from 'react';
+
+function copyToClipboard(text: string): void {
+  navigator.clipboard.writeText(text);
+}
+
+interface CopyUrlButtonProps extends HTMLAttributes<HTMLButtonElement> {
+  value?: string;
+  className?: string;
+}
+
+function CopyUrlButton({ value = 'Copy', className = '' }: CopyUrlButtonProps) {
+  const location = useLocation();
+
+  const handleClick = () => {
+    const url = window.location.origin + location.pathname;
+    copyToClipboard(url);
+  };
+
+  return (
+    <Button
+      onClick={handleClick}
+      className={`${className} border border-border-light rounded-small shadow-none hover:shadow-sm active:shadow-none cursor-pointer`}
+    >
+      {value}
+    </Button>
+  );
+}
+
+export default CopyUrlButton;
