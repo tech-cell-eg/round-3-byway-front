11ab4fbc40e379ca02c4ac55b586daa160ab2d0d
AhmedEssam <ae.devverse@gmail.com>
2025-05-04 18:22:55 +0300

    Feat: Receive Ref to apply qr image download functionality

--- src/components/CommonComponents/QRCodeComponent.tsx
+++ src/components/CommonComponents/QRCodeComponent.tsx
@@ -1,11 +1,24 @@
+import { forwardRef, useRef, useImperativeHandle } from 'react';
 import QRCode from 'react-qr-code';
 
-type QRCodeComponentProps = {
+interface QRCodeComponentProps {
   courseUrl: string;
-};
-
-function QRCodeComponent({ courseUrl }: QRCodeComponentProps) {
-  return <QRCode value={courseUrl} />;
 }
+
+const QRCodeComponent = forwardRef<SVGSVGElement, QRCodeComponentProps>(
+  ({ courseUrl }, ref) => {
+    const containerRef = useRef<HTMLDivElement>(null);
+
+    useImperativeHandle(ref, () => {
+      return containerRef.current?.querySelector('svg') as SVGSVGElement;
+    });
+
+    return (
+      <div ref={containerRef}>
+        <QRCode value={courseUrl} />
+      </div>
+    );
+  }
+);
 
 export default QRCodeComponent;
