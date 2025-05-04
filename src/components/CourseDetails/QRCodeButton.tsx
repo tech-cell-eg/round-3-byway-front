6b427d180fcd1bccf757243e92b2b44ce59912b1
AhmedEssam <ae.devverse@gmail.com>
2025-05-04 19:43:39 +0300

    Feat: Add 'Download as PDF' button

--- src/components/CourseDetails/QRCodeButton.tsx
+++ src/components/CourseDetails/QRCodeButton.tsx
@@ -10,10 +10,11 @@ import {
 import { Button } from '../ui/button';
 import Icon from '../Icon';
 import downloadImage from '@/utils/downloadImage';
+import downloadPdf from '@/utils/downloadPdf';
 
 function QRCodeButton() {
   const [currentUrl, setCurrentUrl] = useState('');
-  const qrRef = useRef<SVGSVGElement | null>(null);
+  const qrRef = useRef<SVGSVGElement>(null);
 
   const handleClick = () => {
     const url = window.location.origin + location.pathname;
@@ -26,31 +27,47 @@ function QRCodeButton() {
     }
   };
 
+  const handleQRPdfDownload = () => {
+    if (qrRef.current) {
+      downloadPdf({ elementRef: qrRef, fileName: 'qr-code' });
+    }
+  };
+
   return (
     <Dialog>
       <DialogTrigger
         onClick={handleClick}
-        className="font-semibold lg:text-xs py-2 px-4 border border-border-light rounded-small hover:shadow-sm active:shadow-none cursor-pointer"
+        className=" py-2 px-4 border border-border-light rounded-small hover:shadow-sm active:shadow-none cursor-pointer"
       >
         QR Code
       </DialogTrigger>
 
       <DialogContent className="bg-surface-light-primary border-none">
         <DialogHeader>
-          <DialogTitle>Course QR Code</DialogTitle>
+          <DialogTitle className="flex justify-center">
+            Course QR Code
+          </DialogTitle>
         </DialogHeader>
 
-        <div className="flex justify-center items-center">
+        <div className="flex justify-center items-center max-w-full">
           <QRCodeComponent courseUrl={currentUrl} ref={qrRef} />
         </div>
 
-        <div className="flex flex-col md:flex-row justify-between items-center gap-3">
+        <div className="flex flex-wrap justify-center md:justify-between items-center gap-3 md:mt-4">
           <Button
             onClick={handleQRImageDownload}
-            className="text-content-dark hover:text-content-light active:text-content-light bg-surface-light-primary hover:bg-accent-secondary active:bg-accent-primary border border-border-light rounded-small shadow-none hover:shadow-sm active:shadow-none cursor-pointer"
+            className="min-w-[160px] text-xs sm:text-base text-content-dark hover:text-content-light active:text-content-light bg-surface-light-primary hover:bg-accent-secondary active:bg-accent-primary border border-border-light rounded-small shadow-none hover:shadow-sm active:shadow-none cursor-pointer"
           >
             <Icon name="download" />
             Download as Image
+          </Button>
+
+          <Button
+            onClick={handleQRPdfDownload}
+            className="min-w-[160px] text-xs sm:text-base text-content-dark hover:text-content-light active:text-content-light bg-surface-light-primary hover:bg-accent-secondary active:bg-accent-primary border border-border-light rounded-small shadow-none hover:shadow-sm active:shadow-none cursor-pointer"
+          >
+            <Icon name="download" />
+            Download as PDF
           </Button>
         </div>
       </DialogContent>
