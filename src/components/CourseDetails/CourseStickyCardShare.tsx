f3805b6d5a693fb75d90fb4577444f15b8377131
AhmedEssam <ae.devverse@gmail.com>
2025-05-04 16:32:19 +0300

    Chore: Add CopyUrlButton and QRCodeButton components

--- src/components/CourseDetails/CourseStickyCardShare.tsx
+++ src/components/CourseDetails/CourseStickyCardShare.tsx
@@ -1,12 +1,16 @@
 import ShareIcons from './ShareIcons';
 import CopyUrlButton from '../CommonComponents/CopyUrlButton';
+import QRCodeButton from './QRCodeButton';
 
 function CourseStickyCardShare() {
   return (
     <div className="flex flex-col gap-2 px-[22px]">
       <div className="flex justify-between items-center">
         <p className="">Share</p>
-        <CopyUrlButton className="font-semibold text-sm " />
+        <div className="flex justfiy-between itesm-center gap-5 lg:gap-2">
+          <CopyUrlButton className="font-semibold text-sm " />
+          <QRCodeButton />
+        </div>
       </div>
       <ShareIcons />
     </div>
